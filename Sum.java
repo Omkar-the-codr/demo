1191203f538419290647a8159f3a25a3ba89c2bb
Omkar Chaturvedi <120505878+Omkar-the-codr@users.noreply.github.com>
2024-08-22 10:26:43 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,7 @@
+public class Sum {
+    public static void main(String[] args) {
+        int num1 = 10; // first number
+        int num2 = 20; // second number
+        int sum = num1 + num2; // adding the two numbers
+
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum
